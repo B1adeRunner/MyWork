40bd3e1383c6c1b0a4b86de46f800bd4fa3f2cd8
B1adeRunner <b1ade.runner@yandex.ru>
2015-10-01 10:38:29 +0600

    Create Main.java

---
+++ Chat/Main.java
@@ -0,0 +1,9 @@
+package ru.korenskiy_alexey;
+
+public class Main {
+
+	public static void main(String[] args) {
+		WindowMainMenu windowMainMenu = new WindowMainMenu();
+		windowMainMenu.buildWindowMainMenu();
+	}
+}
