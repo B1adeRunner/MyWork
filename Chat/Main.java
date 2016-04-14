862ad5468502f98ad1b38eaf8eb38b552beb5e9a
John Doe <johndoe@example.com>
2016-04-14 12:29:25 +0700

    Chat folder is deleted

--- Chat/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.korenskiy_alexey;
-
-public class Main {
-
-	public static void main(String[] args) {
-		WindowMainMenu windowMainMenu = new WindowMainMenu();
-		windowMainMenu.buildWindowMainMenu();
-	}
-}
